1cc1de1a3987687fa2b0ee16733d3f589a79f383
Michael Cooper <mythmon@gmail.com>
2024-11-08 22:49:01 -0800

    remove date from footer

--- observablehq.config.js
+++ observablehq.config.js
@@ -6,4 +6,5 @@ export default {
   header: `<div style="border-bottom: 1px solid grey; display: flex; flex-direction: row-reverse">
     <div><a href="https://github.com/mythmon/workbench">GitHub</a></div>
   </div>`,
+  footer: `Built with <a href="https://observablehq.com/">Observable.</a>`,
 };
